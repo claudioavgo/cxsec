984078f925a7ff7a2ed2b5bb13f693caaafa5e5b
Claudio A <64589027+CodedCA@users.noreply.github.com>
2023-01-10 05:33:32 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <image id="image"></image>
+        <span className="MainName">/CxSec</span>
+        <span className="apresenation">
+          CyberSecurity Researcher
+        </span>
+        <span className="apresenation">
+          Malware Engineer
+        </span>
+        <span className="apresenation">
+          AI Engineer
+        </span>
+        <a className="App-link" href="mailto:i@cxsec.cc" target="_blank" rel="noopener noreferrer">
+          <button className='button'>Mail</button>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
